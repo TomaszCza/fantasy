0e052d1f8e46df804fe5fa4b88cc02aa4a25e92f
ManavRachen15 <manavrachen@hotmail.com>
2023-09-05 18:07:44 +0100

    query find by team name has been added

--- src/main/java/com/fantasy/football/repository/PlayerRepository.java
+++ src/main/java/com/fantasy/football/repository/PlayerRepository.java
@@ -3,5 +3,8 @@ package com.fantasy.football.repository;
 import com.fantasy.football.domain.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface PlayerRepository extends MongoRepository<Player, Integer> {
+    Optional<Player> findByTeam_name(String team_name);
 }
